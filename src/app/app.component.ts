9a4d477cd300f3ffc1bb639fb6dc38a452577ef9
Rasmus Vestergaard Hansen <rasv@danskebank.dk>
2024-03-19 13:11:37 +0100

    init: 2-mvp

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { movies } from '../course-material/dummydata';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
-  title = 'angular-intro-next';
+  movies = movies;
 }
